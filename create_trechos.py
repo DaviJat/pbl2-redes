42aa78c168725820389f9c7dbb161ebb2674bb4e
Gabriel Sena <gabriel.sena.barbosa@gmail.com>
2024-11-03 11:27:54 -0300

    manter os trechos em um unico local

--- create_trechos.py
+++ create_trechos.py
@@ -1,6 +1,6 @@
 import pickle
 
-trechos = [
+trechos_a = [
     {"id": 1, "origem": "São Paulo", "destino": "Rio de Janeiro", "distancia": 430, "quantidade_passagens": 10},
     {"id": 2, "origem": "São Paulo", "destino": "Brasília", "distancia": 1015, "quantidade_passagens": 5},
     {"id": 3, "origem": "São Paulo", "destino": "Salvador", "distancia": 1960, "quantidade_passagens": 3},
@@ -19,37 +19,46 @@ trechos = [
     {"id": 16, "origem": "Rio de Janeiro", "destino": "Recife", "distancia": 2330, "quantidade_passagens": 3}
 ]
 
-trechos = [
-    {"id": 1, "origem": "Salvador", "destino": "São Paulo", "distancia": 1960, "quantidade_passagens": 4},
-    {"id": 2, "origem": "Salvador", "destino": "Rio de Janeiro", "distancia": 1660, "quantidade_passagens": 3},
-    {"id": 3, "origem": "Salvador", "destino": "Manaus", "distancia": 3120, "quantidade_passagens": 1},
-    {"id": 4, "origem": "Salvador", "destino": "Curitiba", "distancia": 2350, "quantidade_passagens": 6},
-    {"id": 5, "origem": "Salvador", "destino": "Recife", "distancia": 800, "quantidade_passagens": 4},
-    {"id": 6, "origem": "Recife", "destino": "Fortaleza", "distancia": 800, "quantidade_passagens": 5},
-    {"id": 7, "origem": "Recife", "destino": "Brasília", "distancia": 1650, "quantidade_passagens": 2},
-    {"id": 8, "origem": "Recife", "destino": "São Paulo", "distancia": 2670, "quantidade_passagens": 3},
-    {"id": 9, "origem": "Recife", "destino": "Porto Alegre", "distancia": 3400, "quantidade_passagens": 2},
-    {"id": 10, "origem": "Recife", "destino": "Manaus", "distancia": 2850, "quantidade_passagens": 1},
+trechos_b = [
+    {"id": 1, "origem": "Rio de Janeiro", "destino": "Porto Alegre", "distancia": 1550, "quantidade_passagens": 2},
+    {"id": 2, "origem": "Brasília", "destino": "Salvador", "distancia": 1440, "quantidade_passagens": 6},
+    {"id": 3, "origem": "Brasília", "destino": "Fortaleza", "distancia": 2200, "quantidade_passagens": 2},
+    {"id": 4, "origem": "Brasília", "destino": "Belo Horizonte", "distancia": 740, "quantidade_passagens": 7},
+    {"id": 5, "origem": "Brasília", "destino": "Manaus", "distancia": 3490, "quantidade_passagens": 1},
+    {"id": 6, "origem": "Brasília", "destino": "Curitiba", "distancia": 1370, "quantidade_passagens": 5},
+    {"id": 7, "origem": "Brasília", "destino": "Recife", "distancia": 2200, "quantidade_passagens": 3},
+    {"id": 8, "origem": "Brasília", "destino": "Porto Alegre", "distancia": 2020, "quantidade_passagens": 4},
+    {"id": 9, "origem": "Salvador", "destino": "Fortaleza", "distancia": 1020, "quantidade_passagens": 3},
+    {"id": 10, "origem": "Salvador", "destino": "Belo Horizonte", "distancia": 1370, "quantidade_passagens": 6},
+    {"id": 11, "origem": "Salvador", "destino": "Manaus", "distancia": 4430, "quantidade_passagens": 2},
+    {"id": 12, "origem": "Salvador", "destino": "Curitiba", "distancia": 2290, "quantidade_passagens": 4},
+    {"id": 13, "origem": "Salvador", "destino": "Recife", "distancia": 800, "quantidade_passagens": 5},
+    {"id": 14, "origem": "Salvador", "destino": "Porto Alegre", "distancia": 3070, "quantidade_passagens": 2},
+    {"id": 15, "origem": "Fortaleza", "destino": "Belo Horizonte", "distancia": 2520, "quantidade_passagens": 3},
+    {"id": 16, "origem": "Fortaleza", "destino": "Manaus", "distancia": 5680, "quantidade_passagens": 1}
 ]
 
-trechos = [
-    {"id": 1, "origem": "Curitiba", "destino": "São Paulo", "distancia": 410, "quantidade_passagens": 7},
-    {"id": 2, "origem": "Curitiba", "destino": "Rio de Janeiro", "distancia": 850, "quantidade_passagens": 4},
-    {"id": 3, "origem": "Curitiba", "destino": "Salvador", "distancia": 2350, "quantidade_passagens": 3},
-    {"id": 4, "origem": "Curitiba", "destino": "Belo Horizonte", "distancia": 1000, "quantidade_passagens": 5},
-    {"id": 5, "origem": "Curitiba", "destino": "Manaus", "distancia": 3600, "quantidade_passagens": 2},
-    {"id": 6, "origem": "Porto Alegre", "destino": "Brasília", "distancia": 1900, "quantidade_passagens": 3},
-    {"id": 7, "origem": "Porto Alegre", "destino": "Recife", "distancia": 3200, "quantidade_passagens": 2},
-    {"id": 8, "origem": "Porto Alegre", "destino": "Fortaleza", "distancia": 3800, "quantidade_passagens": 1},
-    {"id": 9, "origem": "Porto Alegre", "destino": "São Paulo", "distancia": 1115, "quantidade_passagens": 6},
-    {"id": 10, "origem": "Porto Alegre", "destino": "Curitiba", "distancia": 710, "quantidade_passagens": 4},
+trechos_c = [
+    {"id": 1, "origem": "Fortaleza", "destino": "Curitiba", "distancia": 3680, "quantidade_passagens": 2},
+    {"id": 2, "origem": "Fortaleza", "destino": "Recife", "distancia": 800, "quantidade_passagens": 6},
+    {"id": 3, "origem": "Fortaleza", "destino": "Porto Alegre", "distancia": 4570, "quantidade_passagens": 1},
+    {"id": 4, "origem": "Belo Horizonte", "destino": "Manaus", "distancia": 3930, "quantidade_passagens": 2},
+    {"id": 5, "origem": "Belo Horizonte", "destino": "Curitiba", "distancia": 1000, "quantidade_passagens": 4},
+    {"id": 6, "origem": "Belo Horizonte", "destino": "Recife", "distancia": 2090, "quantidade_passagens": 3},
+    {"id": 7, "origem": "Belo Horizonte", "destino": "Porto Alegre", "distancia": 1710, "quantidade_passagens": 5},
+    {"id": 8, "origem": "Manaus", "destino": "Curitiba", "distancia": 4360, "quantidade_passagens": 1},
+    {"id": 9, "origem": "Manaus", "destino": "Recife", "distancia": 5900, "quantidade_passagens": 1},
+    {"id": 10, "origem": "Manaus", "destino": "Porto Alegre", "distancia": 4900, "quantidade_passagens": 1},
+    {"id": 11, "origem": "Curitiba", "destino": "Recife", "distancia": 3030, "quantidade_passagens": 2},
+    {"id": 12, "origem": "Curitiba", "destino": "Porto Alegre", "distancia": 710, "quantidade_passagens": 7},
+    {"id": 13, "origem": "Recife", "destino": "Porto Alegre", "distancia": 3700, "quantidade_passagens": 2}
 ]
 
 with open("trechos_server_a.plk", 'wb') as f:
-    pickle.dump(trechos, f)
+    pickle.dump(trechos_a, f)
 
 with open("trechos_server_b.plk", 'wb') as f:
-    pickle.dump(trechos, f)
+    pickle.dump(trechos_b, f)
 
 with open("trechos_server_c.plk", 'wb') as f:
-    pickle.dump(trechos, f)
+    pickle.dump(trechos_c, f)
